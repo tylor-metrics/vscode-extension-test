4c16a71d0f71f3808a6cca86d9f4228eecf0cdc7
tylor.spence <tylor.spence@metrics.ca>
2024-03-11 17:34:14 -0400

    add custom terminal opened via command

--- src/extension.ts
+++ src/extension.ts
@@ -17,6 +17,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from !');
+
+		vscode.commands.executeCommand('workbench.action.terminal.newWithProfile', {
+            profileName: 'my-custom-terminal'
+        });
+
 	});
 
 	context.subscriptions.push(disposable);
